0a561326ad3fc05791946dd9aae9bbdb719bdb4d
Cksahu143 <charukrishna.sahu@gmail.com>
2026-04-27 11:52:02 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,29 +2,7 @@ import Navbar from "@/components/landing/Navbar";
 import HeroSection from "@/components/landing/HeroSection";
 import ProblemsSection from "@/components/landing/ProblemsSection";
 import FeaturesSection from "@/components/landing/FeaturesSection";
-import AdminSection from "@/components/landing/AdminSection";
-import CTASection from "@/components/landing/CTASection";
-import Footer from "@/components/landing/Footer";
-
-const Index = () => {
-  return (
-    <div className="min-h-screen bg-background">
-      <Navbar />
-      <HeroSection />
-      <ProblemsSection />
-      <FeaturesSection />
-      <AdminSection />
-      <CTASection />
-      <Footer />
-    </div>
-  );
-};
-
-export default Index;
-import Navbar from "@/components/landing/Navbar";
-import HeroSection from "@/components/landing/HeroSection";
-import ProblemsSection from "@/components/landing/ProblemsSection";
-import FeaturesSection from "@/components/landing/FeaturesSection";
+import ScreenshotsSection from "@/components/landing/ScreenshotsSection";
 import AdminSection from "@/components/landing/AdminSection";
 import CTASection from "@/components/landing/CTASection";
 import DownloadSection from "@/components/landing/DownloadSection";
@@ -37,6 +15,7 @@ const Index = () => {
       <HeroSection />
       <ProblemsSection />
       <FeaturesSection />
+      <ScreenshotsSection />
       <AdminSection />
       <CTASection />
       <DownloadSection />
