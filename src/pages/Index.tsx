475f0d32842c3052fe54b3477ea167c44c0394ce
Cksahu143 <charukrishna.sahu@gmail.com>
2026-04-27 09:21:54 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -21,3 +21,28 @@ const Index = () => {
 };
 
 export default Index;
+import Navbar from "@/components/landing/Navbar";
+import HeroSection from "@/components/landing/HeroSection";
+import ProblemsSection from "@/components/landing/ProblemsSection";
+import FeaturesSection from "@/components/landing/FeaturesSection";
+import AdminSection from "@/components/landing/AdminSection";
+import CTASection from "@/components/landing/CTASection";
+import DownloadSection from "@/components/landing/DownloadSection";
+import Footer from "@/components/landing/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <HeroSection />
+      <ProblemsSection />
+      <FeaturesSection />
+      <AdminSection />
+      <CTASection />
+      <DownloadSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
